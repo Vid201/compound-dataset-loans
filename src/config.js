5c9bd0382cb58fc1f7777ab18494947bd26ddd05
Vid Kersic <vid.kersic@yahoo.com>
2022-12-02 09:16:41 +0100

    increase wait time

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const THE_GRAPH_API_URL = 'https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2';
 export const COMPOUND_CTOKEN_API_URL = (blockNumber) => { return `https://api.compound.finance/api/v2/ctoken?meta=true&network=mainnet&block_number=${blockNumber}` };
 export const CSV_PATH = './data.csv';
-export const WAIT_TIME = 500; // 0.5 seconds
+export const WAIT_TIME = 1500; // 1.5 seconds
